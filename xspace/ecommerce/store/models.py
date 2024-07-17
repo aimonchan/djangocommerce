ff8fbcc4cb6ceac78212b6e4813d7df2385e0909
aimonchan <mr.minaimonchan@gmail.com>
2024-07-17 22:16:31 +0630

    Cart Update Codes Updated

--- xspace/ecommerce/store/models.py
+++ xspace/ecommerce/store/models.py
@@ -34,7 +34,15 @@ class Order(models.Model):
     complete= models.BooleanField(default=False,null=True,blank=False)
     transaction_id= models.CharField(max_length=200, null=True)
     def __str__(self):
-        return str(self.id)
+         return str(self.id)
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems= self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
     @property
     def get_cart_total(self):
         orderitems= self.orderitem_set.all()
